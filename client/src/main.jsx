2e4190587ec4e99a713518d65f7db57bbef1b4a7
shibilabasheer <shibila.abasheer@gmail.com>
2025-09-07 10:46:20 +0530

    app.jsx module error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import App from './app.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
